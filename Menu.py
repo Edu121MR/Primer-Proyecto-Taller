c411dd182465e249fc7ed4f306f340603d53f1cd
Edu121MR <e.murillo.1@estudiantec.cr>
2024-03-08 19:41:40 -0600

    Menu

--- Menu.py
+++ Menu.py
@@ -1,20 +1,35 @@
-def menu():
+import time as t
+ListaJugadores = []
+def RegistroJugadores(nombre: str):
+    ListaJugadores.append(nombre)
+
+def Menu():
     while True:
+        print (chr(27) + "[2J")
         print("!!Juego de Memória!!")
-        print("1) Inser")
-        opcionSelec = int(input("Que funcion desea realizar?"))
+        print("1) Registrar Jugadores")
+        print("2) Jugar")
+        print("3) Salir del Juego\n")
+        opt= int(input("Que funcion desea realizar: "))
+        
+        if opt == 1:
+            RegistrarContacto()
+        elif opt == 2:
+            pass
+        elif opt == 3:
+            print("Gracias por jugar")
+            break
+    print (chr(27) + "[2J")
 
-        if opcionSelec == 1:
-            
-        elif opcionSelec == 2:
-            
-        elif opcionSelec == 3:
-            
-        elif opcionSelec == 4:
-            
-        elif opcionSelec == 5:
-            quit()
-        else:
-            input("")
-
-menu()
+def RegistrarContacto():
+    print (chr(27) + "[2J")
+    while True:
+        name=input ("Ingrese el nombre del o los jugadores, cuando termine de escribir los nombres escriba salir:")
+        if name == "salir":
+            break
+        elif name != "salir": 
+            RegistroJugadores(name)
+    print (chr(27) + "[2J")
+    print("Los jugadores registrados son: ", ListaJugadores)   
+    t.sleep(5) 
+Menu()
